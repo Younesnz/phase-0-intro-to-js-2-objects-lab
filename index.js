a66bb1c39fef4f183848422f15af8e39f8df65db
Younes Nourzehi <unesnz@gmail.com>
2023-03-18 23:48:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const employee = 
+{
+    name: "Younes",
+    streetAddress: "Ankara",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key]:value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const tempEmployee = {...employee};
+    delete tempEmployee[key];
+    return tempEmployee;   
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
